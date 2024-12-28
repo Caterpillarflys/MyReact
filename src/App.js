6c49cf2513c704c5f2401383566bcdc7c4dab729
jackHu <12649011+xy_jackhu@user.noreply.gitee.com>
2024-12-28 22:12:12 +0800

    [feature] 父子组件传值

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Son from "./components/son";
 
 function App() {
+  const divStyle = {
+    border: "1px solid red",
+    width: "400px",
+    height: "400px",
+    margin: "20px",
+  };
+  const [name, setName] = useState("无名");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={divStyle}>
+      父：
+      <Son name={name} setSonName={(val) => setName(val)}></Son>
+      <button
+        onClick={() => setName((val) => (val === "无名" ? "有名" : "无名"))}
+      >
+        点击改变名称
+      </button>
     </div>
   );
 }
---
+++ src/components/son.jsx
@@ -0,0 +1,17 @@
+function Son(props) {
+  const divStyle = {
+    border: "1px solid blue",
+    width: "100px",
+    height: "100px",
+    margin: "20px",
+  };
+
+  return (
+    <div style={divStyle}>
+      子：{props.name}
+      <button onClick={() => props.setSonName("子传父")}>子传父：</button>
+    </div>
+  );
+}
+
+export default Son;
